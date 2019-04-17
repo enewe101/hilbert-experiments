tried to fix attention mask bug

evaluation/test_model.py
@@ -1,20 +1,22 @@
 import torch
 import numpy as np
 from evaluation.run_experiments import load_embeddings
-from evaluation.torch_model import BasicAttention, NeuralAttention, build_padding_mask, mask_to_tensor
+from evaluation.torch_model import BasicAttention, NeuralAttention, _build_padding_mask, _mask_to_tensor
 
 
 
 if __name__ == '__main__':
     # X is a list of lists; i.e., a batch of indices with 0 padding
+    padding_id = 50000 + 1
+
     X = torch.LongTensor(
         [
             [1, 2, 3, 4, 5, 6, 7, 8, 9, 123, 44],
-            [10, 20, 30, 40, 50, 60, 70, 80, 90, 3, 0],
-            [2, 4, 6, 8, 10, 12, 14, 16, 18, 12, 0],
-            [1000, 1282, 3321, 4123, 5123, 6342, 7551, 8331, 4329, 0, 0],
-            [9132, 2241, 3234, 4511, 5234, 56, 7222, 8111, 9324, 0, 0],
-            [6523, 2141, 3294, 4411, 523, 561, 4222, 1111, 9323, 0, 0],
+            [10, 20, 30, 40, 50, 60, 70, 80, 90, 3, padding_id],
+            [2, 4, 6, 8, 10, 12, 14, 16, 18, 12, padding_id],
+            [1000, 1282, 3321, 4123, 5123, 6342, 7551, 8331, 4329, padding_id, padding_id],
+            [9132, 2241, 3234, 4511, 5234, 56, 7222, 8111, 9324, padding_id, padding_id],
+            [6523, 2141, 3294, 4411, 523, 561, 4222, 1111, 9323, padding_id, padding_id],
         ]
     )
     pads = torch.LongTensor([0, 1, 1, 2, 2, 2])
@@ -23,25 +25,29 @@ if __name__ == '__main__':
     B, L = len(X), len(X[0])
 
     # padding testing
-    mask = build_padding_mask(B, L, pads)
+    mask = _build_padding_mask(B, L, pads)
     assert (mask.shape == (B, L))
 
     for m, x in zip(mask, X):
         for mij, xij in zip(m, x):
-            assert (mij == np.inf and xij == 0) or (mij != np.inf and xij !=0)
+            assert (mij == np.inf and xij == padding_id) or \
+                   (mij != np.inf and xij != padding_id)
 
-    mT = mask_to_tensor(mask, B)
+    mT = _mask_to_tensor(mask, B)
+    pad_energy = -1e4
+
     assert (mT.shape == (B, L, L))
     for s, energy_mask in enumerate(mT):
         assert (energy_mask.shape == (L, L))
         if pads[s] > 0:
             for j in range(L):
                 for i in range(L):
-                    if i > L - pads[s] or j > L - pads[s]:
-                        assert (energy_mask[i,j] == np.inf)
+                    if i >= L - pads[s] or j >= L - pads[s]:
+                        assert (energy_mask[i,j] == pad_energy)
+                    else:
+                        assert (energy_mask[i,j] != pad_energy)
         else:
-            assert (torch.all(energy_mask == torch.ones((L, L))))
-
+            assert (torch.all(energy_mask == torch.zeros((L, L))))
 
 
     h_embs = load_embeddings('HBT-MLE-FINAL-v50k', device='cpu',

evaluation/torch_model.py
@@ -5,6 +5,9 @@ import torch.nn.functional as F
 from evaluation.hparams import HParams
 
 
+PAD_ENERGY = np.inf
+
+
 def xavier(d):
     return nn.init.xavier_uniform_(torch.zeros((d, d),
                                    device=HParams.DEVICE))
@@ -25,7 +28,7 @@ def get_act_fun(act_str):
         return torch.tanh
     raise NotImplementedError('No activation function \"{}\"!'.format(act_str))
 
-def build_padding_mask(B, L, pads):
+def _build_padding_mask(B, L, pads):
     """ Returns a BxL matrix for padding! """
     ## we will now construct the padding matrix from the pads LongTensor
     P = torch.empty((L, B), device=HParams.DEVICE)  # will transpose after
@@ -39,12 +42,22 @@ def build_padding_mask(B, L, pads):
     # now use np.where to figure out where to put infinities for padding
     mask = torch.where(Z < P,
                        torch.FloatTensor([1]).to(HParams.DEVICE),
-                       torch.FloatTensor([np.inf]).to(HParams.DEVICE))
+                       torch.FloatTensor([PAD_ENERGY]).to(HParams.DEVICE))
     return mask
 
-def mask_to_tensor(mask, bsz):
+def _mask_to_tensor(mask, bsz):
     mT = torch.bmm(mask.unsqueeze(2), mask.reshape(bsz, 1, -1))
+    mT[mT == 1] = 0
+    mT[mT == np.inf] = -1e4
     return mT
+
+def apply_energy_mask(Es, bsz, max_len, pads):
+    mask = _build_padding_mask(bsz, max_len, pads)
+    mT = _mask_to_tensor(mask, bsz)
+    mEs = Es + mT
+    return mEs
+
+
 
 
 # Generic model to feed forward token sequences to embeddings
@@ -208,7 +221,7 @@ class BasicAttention(EmbeddingModel):
                  **kwargs):
 
         super(BasicAttention, self).__init__(h_embs,
-              zero_padding=False, store_covecs=True, **kwargs)
+              zero_padding=True, store_covecs=True, **kwargs)
 
         self.n_classes = n_classes
         self.W = xavier(self.emb_dim) if learn_W else torch.eye(self.emb_dim)
@@ -248,10 +261,7 @@ class BasicAttention(EmbeddingModel):
         Es = torch.bmm(Ks @ self.W, Qs.transpose(1, 2))
 
         # get the mask tensor and apply it to the energy matrix!
-        mask = build_padding_mask(bsz, max_len, pads)
-        mT = mask_to_tensor(mask, bsz)
-        Es *= mT
-        Es[Es == np.inf] *= -1 # unfortunate that we have to do this assignment
+        Es = apply_energy_mask(Es, bsz, max_len, pads)
 
         # now get attention matrices from across the batch
         Ak = self.distr(torch.max(Es, dim=1)[0]).reshape(bsz, 1, max_len)
@@ -280,7 +290,7 @@ class NeuralAttention(EmbeddingModel):
                  **kwargs):
 
         super(NeuralAttention, self).__init__(
-            h_embs, zero_padding=False, store_covecs=True, **kwargs
+            h_embs, zero_padding=True, store_covecs=True, **kwargs
         )
 
         # matrices for neural transformations
@@ -325,10 +335,7 @@ class NeuralAttention(EmbeddingModel):
         Es = torch.bmm(eKs, eQs.transpose(1,2))
 
         # get the mask tensor and apply it to the energy matrix!
-        mask = build_padding_mask(bsz, max_len, pads)
-        mT = mask_to_tensor(mask, bsz)
-        Es *= mT
-        Es[Es == np.inf] *= -1 # unfortunate that we have to do this assignment
+        Es = apply_energy_mask(Es, bsz, max_len, pads)
 
         # now get attention matrices from across the batch
         Ak = self.distr(torch.max(Es, dim=1)[0]).reshape(bsz, 1, max_len)